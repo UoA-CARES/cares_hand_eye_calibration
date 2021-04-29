dcc24cf72557343f58010a32cb46427cd8b2ae19
henry <henry.williams@auckland.ac.nz>
2021-04-29 16:13:32 +1200

    mini rig replacement

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # CARES_hand_eye_calibration
 Hand Eye Calibration for Robot arms and RGB/RGBD cameras
 
+##Dependancies
+In addition to the sensor drivers you want to calibrate with please install visp handeye
+```
+sudo apt-get install ros-$ROS_DISTRO-visp
+```
+
 ## STEREO
 ###STEREO AND HANDEYE
 performs stereo AND hand-eye calibration at same time
--- launch/zivid_handeye.sh
+++ launch/zivid_handeye.sh
@@ -1,6 +1,6 @@
 #ra_ur5!/bin/bash
 #Launch the UR5 communication stuff
-gnome-terminal --tab -- bash -c "s2;roslaunch uoa_ur_description ur5_real.launch limited:=true; exec bash"
+gnome-terminal --tab -- bash -c "s2;roslaunch mini_rig_description ur5_real.launch limited:=true; exec bash"
 #Wait until comms with the UR5 is up and running
 sleep 5
 #Launch move group controller
